536e5e1b2a2c92323062de9a57d0c85926439ab8
abdxu9 <abdou@Xubuntu.myguest.virtualbox.org>
2024-11-18 23:30:22 +0100

    <features> Add a button for the user to change the number of square per line

--- script.js
+++ script.js
@@ -1,29 +1,70 @@
-/* SELECTIONNER LE DIV QUI VA CONTENIR LA GRILLE */
+
 const container = document.querySelector(".container");
+const button = document.querySelector("#grid-setup");
 
-for(let i = 0; i < 16; i++){
-    const lineContainer = document.createElement("div");
-    lineContainer.setAttribute("class",`lineContainer`);
-    container.appendChild(lineContainer);
+function createGrid(numberOfSquare = 16){
+    squareSize = 960/numberOfSquare;
 
-    for(let j = 0; j < 16; j++){
-        const columnDiv = document.createElement("div");
-        columnDiv.setAttribute("class",`column`);
-        lineContainer.appendChild(columnDiv);
+    for(let i = 0; i < numberOfSquare; i++){
+        const lineContainer = document.createElement("div");
+        lineContainer.setAttribute("class",`lineContainer`);
+        lineContainer.style.height = `${squareSize}px`
+        lineContainer.style.width = "960px";
+        container.appendChild(lineContainer);
+
+        for(let j = 0; j < numberOfSquare; j++){
+            const square = document.createElement("div");
+            square.setAttribute("class",`square`);
+            square.style.height = `${squareSize}px`
+            square.style.width = `${squareSize}px`
+            lineContainer.appendChild(square);
+        }
     }
+    detecteButton();
 }
 
-const divSquares = document.querySelectorAll(".column")
-console.log(divSquares);
+function detecteButton(){
+    const squares = document.querySelectorAll(".square");
+    squares.forEach( (square) => {
+        square.addEventListener("mousemove", (e) =>{
+            square.style.background= "rosybrown";
+        })
+        square.addEventListener("mouseout", (e) =>{
+            square.style.background = "red";
+        });
+    });
+}
 
-divSquares.forEach( (divSquare) => {
-    divSquare.addEventListener("mousemove", (e) =>{
-        divSquare.setAttribute("style","background: rosybrown;");
+function createNewGrid(){
+    const lineContainers = document.querySelectorAll(".lineContainer");
+
+    lineContainers.forEach((line) =>{
+        const squares = line.querySelectorAll(".square");
+        squares.forEach((square) =>{
+            line.removeChild(square);
+        })
+        container.removeChild(line);
     })
-    divSquare.addEventListener("mouseout", (e) =>{
-        divSquare.setAttribute("style","background: red;");
-    })
-})
+
+    let numberOfSquare = parseInt(prompt("Number Of Square Per Line: ",16));
+    while(numberOfSquare < 1 || numberOfSquare > 100){
+        numberOfSquare = parseInt(prompt("Number Of Square Per Line: ",16));
+    }
+    createGrid(numberOfSquare);
+    detecteButton();
+
+}
+
+
+createGrid()
+
+
+
+
+
+
+
+
 
 
 /* 
